2ff39c1cdff42a6d0c06e2c115599aba240e8fd6
wasseing <paul.wasson@isen-lille.fr>
2014-05-27 16:56:31 +0200

    Ball class First implementation

--- PongLegacy/PongLegacy/Ball.cs
+++ PongLegacy/PongLegacy/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,26 @@ using System.Text;
 
 namespace PongLegacy
 {
-    class Ball
+    class Ball:SpriteTexture2D
     {
+        public int Radius { get; set; }
+        public Vector2 Speed { get; set; }
+        public Rectangle HitBox{ get; set; }
+        public Pong Pong { get; set; }
 
+        public Ball(Pong pong)
+        {
+
+        }
+
+        public void Update()
+        {
+            
+        }
+        public Boolean checkPlayerColision(Player player)
+        {
+            if (this.HitBox.Intersects(player.HitBox)) ;
+            return false;
+        }
     }
 }
