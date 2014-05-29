880950510db355044f34a1d52d7304370a43987e
wasseing <paul.wasson@isen-lille.fr>
2014-05-29 11:35:02 +0200

    add of missing files

---
+++ PongLegacy/PongLegacy/IPlayerControler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PongLegacy
+{
+    public abstract class IPlayerControler
+    {
+        public Player Player { get; set; }
+        public abstract void MoveUp();
+        public abstract void MoveDown();
+        public abstract void Update();
+
+        public IPlayerControler(Player player)
+        {
+            this.Player = player;
+        }
+    }
+}
---
+++ PongLegacy/PongLegacy/KeyboardPlayerControler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace PongLegacy
+{
+    public class KeyboardPlayerControler : IPlayerControler
+    {
+        public KeyboardState KeyboardState { get; set; }
+        public Keys UpKey { get; set; }
+        public Keys DownKey { get; set; }
+        
+
+
+        public KeyboardPlayerControler(Keys up, Keys down,Player player):base(player)
+        {
+
+            this.KeyboardState = new KeyboardState();
+            this.UpKey = up;
+            this.DownKey = down;
+            
+        }
+
+        public override void Update()
+        {
+            Keys[] Keys = KeyboardState.GetPressedKeys();
+            foreach (Keys key in Keys)
+            {
+                if (key.Equals(this.DownKey))
+                {
+                    this.MoveDown();
+                }
+                if (key.Equals(this.UpKey))
+                {
+                    this.MoveUp();
+                }
+            }
+        }
+
+        public override void MoveUp()
+        {
+            this.Player.MoveUp();
+        }
+        public override void MoveDown()
+        {
+            this.Player.MoveDown();
+        }
+       
+    }
+}
