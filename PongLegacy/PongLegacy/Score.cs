52dcbbb545e6f9c63b515dd0eebffb42e334045d
Thibault Itart-Longueville <tibeoh@gmail.com>
2014-05-30 18:35:20 +0200

    Score is display (font to chnage)

---
+++ PongLegacy/PongLegacy/Score.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PongLegacy
+{
+    public class Score : SpriteText
+    {
+        private int _Value;
+        public int Value
+        {
+            set { this.text = this._Value.ToString(); this._Value = value; }
+            get { return this._Value; }
+        }
+
+        public Score(Vector2 position) : base(position, Color.White, 0.ToString())
+        {
+            this.Value = 0;
+        }
+    }
+}
